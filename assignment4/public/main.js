1ce40af524be66573cb3eca89fafd8f2ca1030ee
Alex Wu <qw2444@nyu.edu>
2026-02-19 12:22:16 -0500

    4

---
+++ assignment4/public/main.js
@@ -0,0 +1,64 @@
+window.onload = function () {
+
+  getTodos();
+
+  document.getElementById("addBtn").addEventListener("click", async function () {
+
+    const input = document.getElementById("todoInput");
+
+    await fetch("/api/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ text: input.value })
+    });
+
+    input.value = "";
+    getTodos();
+  });
+
+  document.getElementById("deleteCompletedBtn").addEventListener("click", async function () {
+    await fetch("/api/todos/completed", {
+      method: "DELETE"
+    });
+    getTodos();
+  });
+
+};
+
+// get data
+async function getTodos() {
+  const response = await fetch("/api/todos");
+  const data = await response.json();
+  renderTodos(data);
+}
+
+function renderTodos(todos) {
+  const list = document.getElementById("todoList");
+  list.innerHTML = "";
+
+  todos.forEach(todo => {
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", async function () {
+      await fetch(`/api/todos/${todo.id}/toggle`, {
+        method: "POST"
+      });
+      getTodos();
+    });
+
+    const textSpan = document.createElement("span");
+    textSpan.textContent = " " + todo.text;
+    if (todo.completed) {
+      textSpan.style.textDecoration = "line-through";
+    }
+
+    li.appendChild(checkbox);
+    li.appendChild(textSpan);
+    list.appendChild(li);
+  });
+}
---
+++ assignment4/server.js
@@ -0,0 +1,71 @@
+//library import
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//instance of express class
+const app = express();
+
+//allow the use of my static files (front-end code)
+app.use(express.static('public'));
+
+// const parser = bodyParser.urlencoded({extended: true});
+// app.use(parser);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let todos = [
+//   { id: 1, text: "Finish homework", completed: false },
+//   { id: 2, text: "Go to gym", completed: false }
+];
+
+//setting up
+app.get("/api/todos", (request, response) => {
+  response.json(todos);
+});
+
+app.post("/api/todos", (request, response) => {
+  const newTodo = {
+    id: Date.now(),
+    text: request.body.text,
+    completed: false
+  };
+
+  todos.push(newTodo);
+  response.json(newTodo);
+});
+
+app.post("/api/todos/:id/toggle", (request, response) => {
+  const id = parseInt(request.params.id, 10);
+  if (Number.isNaN(id)) {
+    return response.status(400).json({ message: "Invalid todo id" });
+  }
+
+  const todo = todos.find(t => t.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ message: "Not found" });
+  }
+
+  todo.completed = !todo.completed;
+  response.json(todo);
+});
+
+app.delete("/api/todos/completed", (request, response) => {
+  todos = todos.filter(todo => !todo.completed);
+  response.json({ message: "Completed todos deleted" });
+});
+
+app.delete("/api/todos/:id", (request, response) => {
+  const id = parseInt(request.params.id, 10);
+  if (Number.isNaN(id)) {
+    return response.status(400).json({ message: "Invalid todo id" });
+  }
+
+  todos = todos.filter(todo => todo.id !== id);
+  response.json({ message: "Deleted" });
+});
+
+//start our express application
+app.listen(3000,() => {
+  console.log("start server is working");
+});
